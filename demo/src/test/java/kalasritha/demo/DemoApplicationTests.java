15c06f8330eee7581f3a9c82197cc8c8b7de07f2
root <root@Kalasritha.>
2025-10-08 10:42:52 +0000

    file added

---
+++ demo/src/test/java/kalasritha/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kalasritha.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
